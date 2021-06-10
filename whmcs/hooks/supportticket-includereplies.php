510d063db49484e6c353f2e6773a01455f2da007
Dave Browning <dave@dlbnetworks.com>
2021-06-11 03:42:18 +1000

    Update supportticket-includereplies.php

--- whmcs/hooks/supportticket-includereplies.php
+++ whmcs/hooks/supportticket-includereplies.php
@@ -2,17 +2,14 @@
 
 use WHMCS\Database\Capsule;
 
-add_hook('EmailPreSend', 1, function($vars)
-{
-    if (in_array($vars['messagename'], array('Support Ticket Reply')))
-    {
+add_hook('EmailPreSend', 1, function($vars) {
+    if (in_array($vars['messagename'], array('Support Ticket Reply'))) {
 	$ticketid = $vars['relid'];
 	$replies = Capsule::select(Capsule::raw('SELECT * FROM tblticketreplies WHERE tid = "' . $vars['relid'] . '" ORDER BY id DESC'));
 	$companyname = Capsule::select(Capsule::raw('SELECT * FROM tblconfiguration WHERE setting = "CompanyName"'));
 	$companyname = $companyname[0]->value;
 	$r = 0;
-        foreach ($replies as $v)
-        {
+        foreach ($replies as $v) {
 		if ($r <> 0) {
 			if (empty($v->admin)) {
 				if (empty($v->email)) {
